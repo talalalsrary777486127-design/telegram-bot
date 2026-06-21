94a5f7ceb32f4f723dae1b2f0cde625923be4c19
talalalsrary777486127-design <talalalsrary777486127@gmail.com>
2026-06-21 16:23:58 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,9 +1,30 @@
 import os
 
-BOT_TOKEN = os.getenv("TELEGRAM_BOT_TOKEN")
 
-ADMIN_USER_ID = int(
-    os.getenv("ADMIN_USER_ID", "0")
+BOT_TOKEN = os.getenv(
+    "TELEGRAM_BOT_TOKEN"
 )
 
-MAX_FILE_SIZE = 50 * 1024 * 1024
+ADMIN_USER_ID = int(
+    os.getenv(
+        "ADMIN_USER_ID",
+        "0"
+    )
+)
+
+DATABASE_PATH = os.getenv(
+    "DATABASE_PATH",
+    "links.db"
+)
+
+MAX_FILE_SIZE = int(
+    os.getenv(
+        "MAX_FILE_SIZE",
+        50 * 1024 * 1024
+    )
+)
+
+BACKUP_FOLDER = os.getenv(
+    "BACKUP_FOLDER",
+    "backups"
+)
